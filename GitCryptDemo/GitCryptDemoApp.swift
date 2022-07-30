366bedccdfe073427bc684fd6ed3b06c4bb6ce56
Peter van den Hamer <github@vdhamer.com>
2022-07-31 00:30:35 +0200

    Initial commit: pretty empty project so far.

---
+++ GitCryptDemo/GitCryptDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCryptDemoApp.swift
+//  GitCryptDemo
+//
+//  Created by Peter van den Hamer on 30/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitCryptDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
